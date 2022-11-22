01f5b1ff728d6fa48aa245b09971e896becd9399
Syce Gaming <77421128+SyceGaming@users.noreply.github.com>
2022-11-21 22:43:47 -0500

    new cdn

--- game_data.js
+++ game_data.js
@@ -1,5 +1,5 @@
 let lastUpdate = "11/3/2022 (v5.1.0)"
-let cdnUrl = "celebrated-stardust-91ad96.netlify.app"
+let cdnUrl = "https://celebrated-stardust-91ad96.netlify.app"
 //CHANGE IMAGES ON HOME PAGE TOO
 const data = [
     {
