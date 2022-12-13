df100fb8e8daa6fcd563bf308719ba0f001a3791
Syce Gaming <77421128+SyceGaming@users.noreply.github.com>
2022-12-12 23:28:51 -0500

    hotfix?

--- game_data.js
+++ game_data.js
@@ -2360,7 +2360,7 @@ function search(){
     let x = document.getElementsByClassName("container")
 
     for(i = 0; i < x.length; i++){
-        if(!data[i].name.toLowerCase().includes(input)){
+        if(!x[[i].getElementByClassName("game_title")[0].textContent.toLowerCase().includes(input)){
             x[i].style.display = "none";
         } else {
             x[i].style.display = "inline-table"
